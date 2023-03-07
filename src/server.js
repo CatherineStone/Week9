9ae93f368b92d3309782ff5d1bd4c4c6bbe5ad63
CatherineStone <106274539+CatherineStone@users.noreply.github.com>
2023-03-07 15:36:46 +0000

    routes setup

--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 
-const exampleRouter = require("../middleWareExample");
-
 const port = process.env.PORT || 5001;
+
+const userRouter = require("./users/routes")
+
+const User = require("./users/model")
 
 const app = express();
 
 app.use(express.json());
 
-app.use(exampleRouter);
+app.use(userRouter);
+
+const syncTables = () => {
+    User.sync({ alter: true, force: false });
+}
 
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "API is working." })
 })
 
 app.listen(port, () => {
+    syncTables();
     console.log(`Server is listening on port ${port}.`)
 });
